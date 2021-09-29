7fac1d54c6fe77bf71019431bac500096de783a2
Sasindu Alahakoon <47134066+SasinduDilshara@users.noreply.github.com>
2021-09-29 21:26:59 +0530

    Add Foreground Threads

--- Social_DIstance_Reminder/app/src/main/java/com/example/social_distance_reminder/UI/HomeActivity.java
+++ Social_DIstance_Reminder/app/src/main/java/com/example/social_distance_reminder/UI/HomeActivity.java
@@ -20,16 +20,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        if (!closed) {
-//            startService(new Intent(this, BackgroundServiceHandler.class));
-            try {
-                Thread.sleep(1000);
-                closed = true;
-//                stopService(new Intent(this, BackgroundServiceHandler.class));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        if(!closed) startService(new Intent(this, BackgroundServiceHandler.class));
+        closed = true;
     }
 
     public void sendTestNotification(View view) {
--- Social_DIstance_Reminder/app/src/main/java/com/example/social_distance_reminder/services/BackgroundServiceHandler.java
+++ Social_DIstance_Reminder/app/src/main/java/com/example/social_distance_reminder/services/BackgroundServiceHandler.java
@@ -9,7 +9,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Icon;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
@@ -19,56 +24,77 @@ import com.example.social_distance_reminder.UI.HomeActivity;
 import static com.example.social_distance_reminder.helper.RandomIDGenerator.getBackgroundNotifictionID;
 
 public class BackgroundServiceHandler extends Service {
-    private static final int NOTIF_ID = 1;
-    private static final String NOTIF_CHANNEL_ID = "Channel_Id";
-    private static boolean stillRunning = false;
+    private Looper serviceLooper;
+    private ServiceHandler serviceHandler;
+    public static int i = 0;
+    public static boolean isRunning = false;
 
-    public static boolean isStillRunning() {
-        return stillRunning;
+    // Handler that receives messages from the thread
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            // Normally we would do some work here, like download a file.
+            // For our sample, we just sleep for 5 seconds.
+            System.out.println("Here we come");
+            while(i < 30) {
+                System.out.println("Executing "+ i + "\n");
+                i = i + 1;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // Stop the service using the startId, so that we don't stop
+            // the service in the middle of handling another job
+            stopSelf(msg.arg1);
+        }
     }
 
-    public static void setStillRunning(boolean stillRunning) {
-        BackgroundServiceHandler.stillRunning = stillRunning;
+    @Override
+    public void onCreate() {
+        // Start up the thread running the service. Note that we create a
+        // separate thread because the service normally runs in the process's
+        // main thread, which we don't want to block. We also make it
+        // background priority so CPU-intensive work doesn't disrupt our UI.
+        HandlerThread thread = new HandlerThread("ServiceStartArguments");
+        thread.start();
+
+        // Get the HandlerThread's Looper and use it for our Handler
+        serviceLooper = thread.getLooper();
+        serviceHandler = new ServiceHandler(serviceLooper);
     }
 
-    @Nullable
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+
+        Notification notification = NotificationHelperService
+                .createBackgroundNotificationForService("Running","App is running in background", getApplicationContext());
+        startForeground(getBackgroundNotifictionID(), notification);
+
+        // For each start request, send a message to start a job and deliver the
+        // start ID so we know which request we're stopping when we finish the job
+        Message msg = serviceHandler.obtainMessage();
+        msg.arg1 = startId;
+        serviceHandler.sendMessage(msg);
+
+        // If we get killed, after returning from here, restart
+        //TODO: Read the document and fix the stack
+        return START_STICKY;
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
+        // We don't provide binding, so return null
         return null;
     }
 
-    public void test() {
-        int i = 0;
-        while (true) {
-            System.out.println(i + "\n");
-            i += 1;
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            if ( i == 700) {
-                stopForeground(true);
-                stillRunning = false;
-                break;
-            }
-        }
-    }
-
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId){
-        int value = super.onStartCommand(intent, flags, startId);
-        if (!stillRunning) {
-            startForeground(getApplicationContext());
-            test();
-            stillRunning = true;
-        }
-        return value;
-    }
-
-    private void startForeground(Context context) {
-        Notification notification = NotificationHelperService
-                .createBackgroundNotificationForService("Running","App is running in background", context);
-        startForeground(getBackgroundNotifictionID(), notification);
+    public void onDestroy() {
+        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
     }
 }
